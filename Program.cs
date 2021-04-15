dd9090af6517fe5ce2bbb58c92ee4e04fe670e62
jancaitis <79664863+jancaitis@users.noreply.github.com>
2021-04-15 13:10:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace projektz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter number of arrays: ");
+            string inputarray = Console.ReadLine();
+            int numberofarray = Convert.ToInt32(inputarray);
+            string[] arrayz = new string[numberofarray];
+            string sep = "";
+            string beigas = "";
+
+            for (int i = 0; i < numberofarray; i++)
+            {
+                Console.Write("write some words to collect: ");
+                string newinput = Console.ReadLine();
+                string newArray = Convert.ToString(newinput);
+                arrayz[i] = newArray;
+                
+            }
+         
+            for (int i = 0; i < numberofarray; i++)
+            {  
+                if (!(beigas.Contains(arrayz[i])))
+                {
+               beigas += sep + arrayz[i];
+               sep = ",";
+                }
+            }
+            
+            Console.Write($"{beigas}");
+        }
+    }
+}
